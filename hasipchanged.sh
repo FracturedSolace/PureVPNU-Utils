47c0751c96aa8bc92eb95eb99e3f2108e100c8ff
FracturedSolace <piscesgeek219@gmail.com>
2021-08-12 16:57:37 -0700

    Added protection in hasipchanged.sh for certain utilities not being installed

--- hasipchanged.sh
+++ hasipchanged.sh
@@ -4,6 +4,20 @@
 #------------
 #This script checks currnt IP against last known region to ensure that the IP has actually changed after a VPN switch
 
+if ! command -v geoiplookup &> /dev/null
+then
+	echo ["$(tput setaf 1)"ERROR"$(tput setaf 7)"] geoiplookup does not appear to be installed
+	echo   try pacman -Sy geoip
+	exit 1
+fi
+
+if ! command -v dig &> /dev/null
+then
+	echo ["$(tput setaf 1)"ERROR"$(tput setaf 7)"] dig does not appear to be installed
+	echo   try pacman -Sy dnsutils
+	exit 1
+fi
+
 new_ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 new_region="$(geoiplookup ${new_ip})"
 
@@ -19,5 +33,5 @@ else
 	exit 0
 fi
 
-#This should never be reached, but exists as a safety
+#This should never be reached, but exists as a safety against returning 0 on failure
 exit 1
